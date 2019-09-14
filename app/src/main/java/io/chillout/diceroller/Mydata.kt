da834b5a4b4ac834be6d5bca5df8df01d18efcfc
mage <mag@localhost.localdomain>
2019-09-14 23:28:16 +0530

    lol

--- app/src/main/java/io/chillout/diceroller/Mydata.kt
+++ app/src/main/java/io/chillout/diceroller/Mydata.kt
@@ -2,4 +2,4 @@ package io.chillout.diceroller
 
 data class Mydata (var someRandom: String = "",var nickname: String = "")
 
-data class Mynick(var Nickname:String = "", var dicerool: Int = 0)
+data class Mynick (var Nickname:String = "", var dicerool: Int = 0)
